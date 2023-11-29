ef93cb3d38e76b6c9032813f96ab694bed2f7576
shreySaxena <shreyyerhs07@gmail.com>
2023-11-29 11:03:41 +0530

    added single class dependencies package

---
+++ src/main/java/org/example/singleClassDependencies/Main.java
@@ -0,0 +1,12 @@
+package org.example.singleClassDependencies;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext=new ClassPathXmlApplicationContext("singleClassDependencies.xml");
+        Student student=(Student) applicationContext.getBean("student");
+        System.out.println(student);
+    }
+}
